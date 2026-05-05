e19807f2522bf8c3b828ad5bdfdff62b2fddaee0
SHANMUGA SREE VYSHNAV <Shanmukhasri00@gmail.com>
2026-05-05 12:01:33 +0530

    Uncomment next and previous arrow buttons in slider

--- src/components/PlanetsSlider/planetSlide.js
+++ src/components/PlanetsSlider/planetSlide.js
@@ -10,14 +10,14 @@ const PlanetsSlider = (props) => {
     dot: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // nextArrow: <button className="slick-next">Next</button>,
-    // prevArrow: <button className="slick-prev">Prev</button>,
+    nextArrow: <button className="slick-next">Next</button>,
+    prevArrow: <button className="slick-prev">Prev</button>,
   };
   const { planetsList } = props;
   return (
     <div className="planets-container">
       <Slider {...set} className="slider-container">
-        {planetsList.map((eachPlanet) => (
+        {planetsList.map((eachPlanet => (
           <PlanetItem planetList={eachPlanet} key={eachPlanet.id} />
         ))}
       </Slider>
